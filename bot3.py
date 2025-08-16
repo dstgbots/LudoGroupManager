b0feb8542e32013fdec1eb3429b353ac22b41f88
dstgbots <138157172+dstgbots@users.noreply.github.com>
2025-08-16 12:40:10 +0530

    Update bot3.py

--- bot3.py
+++ bot3.py
@@ -3173,7 +3173,7 @@ async def main():
     API_ID = 18274091
     API_HASH = "97afe4ab12cb99dab4bed25f768f5bbc"
     GROUP_ID = -1002849354155
-    ADMIN_IDS = [2109516065]
+    ADMIN_IDS = [2109516065, 739290618]
     
     print(f"🚀 Starting Ludo Manager Bot...")
     print(f"🔑 Bot Token: {BOT_TOKEN[:20]}...")
